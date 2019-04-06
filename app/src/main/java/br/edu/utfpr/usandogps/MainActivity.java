bc8b1fa33f9aa4716eba0b3da567f4598339468d
Weslei Patrick <wesleipatrick@gmail.com>
2019-04-06 09:47:32 -0300

    Bind de campos

--- app/src/main/java/br/edu/utfpr/usandogps/MainActivity.java
+++ app/src/main/java/br/edu/utfpr/usandogps/MainActivity.java
@@ -5,9 +5,17 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tvLatitude;
+    private TextView tvLongitude;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        tvLatitude = findViewById( R.id.tvLatitude );
+        tvLongitude = findViewById( R.id.tvLongitude );
+
     }
 }
