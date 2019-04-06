84ad434c841a2d84ce99e7b81fb95405d59f9538
Carlos Vinícius Ferrareze <viniferrareze@gmail.com>
2019-04-06 11:19:53 -0300

    Concluido

--- app/src/main/java/br/edu/utfpr/usandogps/MainActivity.java
+++ app/src/main/java/br/edu/utfpr/usandogps/MainActivity.java
@@ -2,14 +2,17 @@ package br.edu.utfpr.usandogps;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements LocationListener {
 
@@ -27,6 +30,15 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
         LocationManager lm = (LocationManager) getSystemService( Context.LOCATION_SERVICE);
 
+        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+        if (! lm.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            Toast.makeText(this, "Habilitar GPS", Toast.LENGTH_LONG).show();
+
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
 
             return;
@@ -34,7 +46,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
         lm.requestLocationUpdates( LocationManager.NETWORK_PROVIDER,1000, 0, this );
 
-        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
     }
 
     @Override
